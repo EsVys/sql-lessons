da6bee756cfacc14e429aea2906134f9fac559f9
EsVys <108893515+EsVys@users.noreply.github.com>
2022-09-10 21:59:22 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `Patient` (
+  `PatientId` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Birthdate` DATETIME NOT NULL
+  );
+  
+ CREATE TABLE IF NOT EXIST `Doctor` (
+   `DoctorId` int primary key auto_increment NOT NULL,
+   `Name` varchar(255) NOT NULL,
+   `Field` varchar(255) NOT NULL,
+   );
+   
+ CREATE TABLE IF NOT EXIST `Appointment` (
+   `Date` DATETIME NOT NULL,
+   `DoctorId` int NOT NULL,
+   `PatientId` int NOT NULL,
+   );
