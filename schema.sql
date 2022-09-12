edfd2a021900d78c475578564ac4e653502956bb
EsVys <108893515+EsVys@users.noreply.github.com>
2022-09-12 15:23:29 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -23,12 +23,12 @@ CREATE TABLE IF NOT EXISTS `Patient` (
    ('Elen Fitzgerald', 'Chirurg');
    
  INSERT INTO `Patient` (`Name`, `Address`, `Birthdate`) VALUES
- 	('Gerta Hanes', 'Mirror Strasse 9', '1989-02-02 15:55:00'),
+ 	  ('Gerta Hanes', 'Mirror Strasse 9', '1989-02-02 15:55:00'),
     ('Ivan Johanovic', 'Nona Street 12', '1966-06-06 06:06:00'),
     ('Karen Lane', 'Opal Avenue 103', '1948-07-12 10:09:00');
     
  INSERT INTO `Appointment` (`Date`, `DoctorId`, `PatientId`) VALUES
- 	('2023-05-05 10:25:00', '1', '3'),
+ 	  ('2023-05-05 10:25:00', '1', '3'),
     ('2022-03-06 09:12:00', '2', '2'),
     ('2023-09-08 06:55:00', '3', '1');
  	
